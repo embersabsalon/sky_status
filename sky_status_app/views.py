62c30c026b0e37609ffe5f61318f5b6f2da7ce62
embersabsalon <embersabsalon@protonmail.com>
2020-12-01 16:23:53 +0800

    Fix searching

--- sky_status_app/views.py
+++ sky_status_app/views.py
@@ -9,9 +9,11 @@ from sky_status_app.models.city import City
 
 
 def index(request):
-    city_name = request.GET.get('city', '')
-    city = City.objects.filter(name__contains=city_name).first()
     context = {}
+    city = None
+    city_name = request.GET.get('city', '')
+    if city_name:
+        city = City.objects.filter(name__contains=city_name).first()
     if city:
         weather_url = f'http://api.openweathermap.org/data/2.5/weather?id={city.city_id}&appid={settings.OPEN_WEATHER_API_KEY}'
         r = requests.get(weather_url)
