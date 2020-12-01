b4f29223cdcb8688fdfdba3db1f98ca3ab61bca6
embersabsalon <embersabsalon@protonmail.com>
2020-12-01 15:26:32 +0800

    Add searching capability

--- sky_status_app/views.py
+++ sky_status_app/views.py
@@ -9,15 +9,21 @@ from sky_status_app.models.city import City
 
 
 def index(request):
-    weather_url = f'http://api.openweathermap.org/data/2.5/weather?id=1717512&appid={settings.OPEN_WEATHER_API_KEY}'
-    r = requests.get(weather_url)
-    loaded_data = json.loads(r.text)
-    weather_data = {
-        'name': loaded_data['name'],
-        'weather': loaded_data['weather'][0]['main'],
-        'icon': loaded_data['weather'][0]['icon'],
-        'wind': loaded_data['wind']['speed'],
-        'temp': loaded_data['main']['temp']
-    }
-    context = {'weather_data': weather_data}
+    city_name = request.GET.get('city', '')
+    city = City.objects.filter(name__contains=city_name).first()
+    context = {}
+    if city:
+        weather_url = f'http://api.openweathermap.org/data/2.5/weather?id={city.city_id}&appid={settings.OPEN_WEATHER_API_KEY}'
+        r = requests.get(weather_url)
+        loaded_data = json.loads(r.text)
+        weather_data = {
+            'name': loaded_data['name'],
+            'weather': loaded_data['weather'][0]['main'],
+            'icon': loaded_data['weather'][0]['icon'],
+            'wind': loaded_data['wind']['speed'],
+            'temp': loaded_data['main']['temp']
+        }
+        context = {'weather_data': weather_data}
+    elif city_name and not city:
+        context = {'message': 'City not found.'}
     return render(request, 'sky_status_app/index.html', context)
