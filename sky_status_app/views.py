95c0c255e4ac51dd0aeabe83890cc58a1207b68c
embersabsalon <embersabsalon@protonmail.com>
2020-12-01 13:59:51 +0800

    Add select sky status

--- sky_status_app/views.py
+++ sky_status_app/views.py
@@ -1,5 +1,21 @@
+import requests
+import json
+
 from django.shortcuts import render
+from django.conf import settings
 
 
 def index(request):
-    return render(request, 'sky_status_app/index.html')
+    weather_url = f'http://api.openweathermap.org/data/2.5/weather?id=1717512&appid={settings.OPEN_WEATHER_API_KEY}'
+    r = requests.get(weather_url)
+    loaded_data = json.loads(r.text)
+    weather_data = {
+        'name': loaded_data['name'],
+        'weather': loaded_data['weather'][0]['main'],
+        'icon': loaded_data['weather'][0]['icon'],
+        'wind': loaded_data['wind']['speed'],
+        'temp': loaded_data['main']['temp']
+    }
+    print (weather_data)
+    context = {'weather_data': weather_data}
+    return render(request, 'sky_status_app/index.html', context)
